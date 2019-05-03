50760b2a691f3a75b36bb293146703a1537edf95
Huanrui Yang <hy128@hl279-cmp-01.egr.duke.edu>
2019-05-03 13:29:31 -0400

    Unify command and output with pruning code

--- imagenet/variance.py
+++ imagenet/variance.py
@@ -8,6 +8,13 @@ import numpy as np
 import sys
 #sys.path.append('/home/hy128/github/vision/')
 
+import matplotlib
+matplotlib.use("pdf")
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+import logging
+
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -52,13 +59,19 @@ parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
+parser.add_argument('--lr_decay', '--learning-rate-decay', default=0.1, type=float,
+                    metavar='LRD', help='learning rate decay')
+parser.add_argument('--lr_int', '--learning-rate-interval', default=30, type=int,
+                    metavar='LRI', help='learning rate decay interval')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=100, type=int,
                     metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='saves/elt_0.0_0', type=str, metavar='PATH',
+parser.add_argument('--resume', default='saves', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--model', default='model', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
@@ -77,12 +90,23 @@ parser.add_argument('--gpu', default=None, type=int,
 parser.add_argument('--sensitivity', type=float, default=1e-4,
                     help="sensitivity value that is used as threshold value")
 
+global args, best_prec1, save_path
+args = parser.parse_args()
 best_prec1 = 0
 
-def main():
-    global args, best_prec1
-    args = parser.parse_args()
+save_path = os.path.join(args.resume, 'finetune', datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+else:
+    raise OSError('Directory {%s} exists. Use a new one.' % save_path)
 
+logging.basicConfig(filename=os.path.join(save_path, 'log.txt'), level=logging.INFO)
+logger = logging.getLogger('main')
+logger.addHandler(logging.StreamHandler())
+logger.info("Saving to %s", save_path)
+logger.info("Running arguments: %s", args)
+
+def main():
     if args.seed is not None:
         random.seed(args.seed)
         torch.manual_seed(args.seed)
@@ -131,7 +155,7 @@ def main():
                                 weight_decay=args.weight_decay)
     
     if args.pretrained is not True:
-        model.load_state_dict(torch.load(args.resume+'.pth'))
+        model.load_state_dict(torch.load(os.path.join(args.resume, args.model+'.pth')))
         print('model loaded')
 
     cudnn.benchmark = True
@@ -189,14 +213,18 @@ def main():
             new_mask = np.where(abs(tensor) < threshold, 0, tensor)
             p.data = torch.from_numpy(new_mask).to(device)        
 
-    util.print_nonzeros(model)
+    #util.print_nonzeros(model)
     print('Pruned model evaluation...')
-    prec1 = validate(val_loader, model, criterion)
+    prec1, prec5 = validate(val_loader, model, criterion)
     
-    best_prec1 = prec1
-    torch.save(model.state_dict(), args.resume+'_V_'+str(args.sensitivity)+'.pth')
+    best_prec1 = prec5
+    torch.save(model.state_dict(), os.path.join(save_path, 'finetuned.pth'))
 
     print("--- Finetuning ---")
+    
+    curves = np.zeros(((args.epochs-args.start_epoch)*(len(train_loader)//args.print_freq),2))
+    valid = np.zeros(((args.epochs-args.start_epoch),3))
+    step = 0
 
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
@@ -204,21 +232,67 @@ def main():
         adjust_learning_rate(optimizer, epoch)
 
         # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch)
+        curves,step = train(train_loader, model, criterion, optimizer, epoch, curves, step)
 
         # evaluate on validation set
-        prec1 = validate(val_loader, model, criterion)
-        if prec1 > best_prec1:
-            torch.save(model.state_dict(), args.resume+'_T_'+str(args.sensitivity)+'.pth')
-            best_prec1 = prec1
+        valid[epoch, 0] = epoch
+        valid[epoch, 1], valid[epoch, 2] = validate(val_loader, model, criterion)
+        prec5 = valid[epoch, 2]
+        
+        # plot training curve
+        np.savetxt(os.path.join(save_path, 'curves.dat'), curves)
+        
+        clr1 = (0.5, 0., 0.)
+        clr2 = (0.0, 0.5, 0.)
+        fig, ax1 = plt.subplots()
+        ax1.set_xlabel('steps')
+        ax1.set_ylabel('Loss', color=clr1)
+        ax1.tick_params(axis='y', colors=clr1)
+        
+        start = 0
+        end = step
+        markersize = 12
+        coef = 2.
+        ax1.plot(curves[start:end, 0], curves[start:end, 1], '--', color=[c*coef for c in clr1], markersize=markersize)
+        
+        #ax2.set_ylim(bottom=20, top=100)
+        ax1.legend(('Train loss'), loc='lower right')
+        fig.savefig(os.path.join(save_path, 'loss-vs-steps.pdf'))
+        
+        # plot validation curve
+        np.savetxt(os.path.join(save_path, 'valid.dat'), valid)
+        
+        fig3, ax5 = plt.subplots()
+        ax6 = ax5.twinx()
+        ax5.set_xlabel('epochs')
+        ax5.set_ylabel('Acc@1', color=clr1)
+        ax5.tick_params(axis='y', colors=clr1)
+        ax6.set_ylabel('Acc@5', color=clr2)
+        ax6.tick_params(axis='y', colors=clr2)
+        
+        start = 0
+        end = epoch+1
+        markersize = 12
+        coef = 2.
+        ax5.plot(valid[start:end, 0], valid[start:end, 1], '--', color=[c*coef for c in clr1], markersize=markersize)
+        ax6.plot(valid[start:end, 0], valid[start:end, 2], '-', color=[c*coef for c in clr2], markersize=markersize)
+        
+        #ax2.set_ylim(bottom=20, top=100)
+        ax5.legend(('Acc@1'), loc='lower right')
+        ax6.legend(('Acc@5'), loc='lower left')
+        fig3.savefig(os.path.join(save_path, 'accuracy-vs-epochs.pdf'))
+        
+        if prec5 > best_prec1:
+            torch.save(model.state_dict(), os.path.join(save_path, 'finetuned.pth'))
+            best_prec1 = prec5
             print('New best performance')
             
     print("--- Evaluating ---")        
-    model.load_state_dict(torch.load(args.resume+'_V_'+str(args.sensitivity)+'.pth'))
+    model.load_state_dict(torch.load(os.path.join(save_path, 'finetuned.pth')))
     prec1 = validate(val_loader, model, criterion)
 
 
-def train(train_loader, model, criterion, optimizer, epoch):
+def train(train_loader, model, criterion, optimizer, epoch, curves, step):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
@@ -265,8 +339,13 @@ def train(train_loader, model, criterion, optimizer, epoch):
         batch_time.update(time.time() - end)
         end = time.time()
         #break
-        if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
+        if i and i % args.print_freq == 0:
+            curves[step, 0] = len(train_loader)*epoch+i
+            curves[step, 1] = losses.avg
+            
+            step += 1
+                   
+            logger.info('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
@@ -274,6 +353,9 @@ def train(train_loader, model, criterion, optimizer, epoch):
                   'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                    epoch, i, len(train_loader), batch_time=batch_time,
                    data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+    return curves, step
+
 
 
 def validate(val_loader, model, criterion):
@@ -309,7 +391,7 @@ def validate(val_loader, model, criterion):
             end = time.time()
 
             if i % 100 == 0:
-                print('Test: [{0}/{1}]\t'
+                logger.info('Test: [{0}/{1}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                       'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
@@ -317,10 +399,46 @@ def validate(val_loader, model, criterion):
                        i, len(val_loader), batch_time=batch_time, loss=losses,
                        top1=top1, top5=top5))
 
-        print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
 
-    return top1.avg
+        nonzero = total = 0
+        filter_count = filter_total = 0
+        total_sparsity = total_layer = 0
+        for name, p in model.named_parameters():
+            if 'weight' in name and len(list(p.size()))>1:
+                tensor = p.data.cpu().numpy()
+                tensor = np.abs(tensor)
+                nz_count = np.count_nonzero(tensor)
+                total_params = np.prod(tensor.shape)
+                nonzero += nz_count
+                total += total_params
+                
+                if len(tensor.shape)==4:
+                    dim0 = np.sum(np.sum(tensor, axis=0),axis=(1,2))
+                    dim1 = np.sum(np.sum(tensor, axis=1),axis=(1,2))
+                    nz_count0 = np.count_nonzero(dim0)
+                    nz_count1 = np.count_nonzero(dim1)
+                    filter_count += nz_count0*nz_count1
+                    filter_total += len(dim0)*len(dim1)
+                    total_sparsity += 1-(nz_count0*nz_count1)/(len(dim0)*len(dim1))
+                    total_layer += 1
+                if len(tensor.shape)==2:
+                    dim0 = np.sum(tensor, axis=0)
+                    dim1 = np.sum(tensor, axis=1)
+                    nz_count0 = np.count_nonzero(dim0)
+                    nz_count1 = np.count_nonzero(dim1)
+                    filter_count += nz_count0*nz_count1
+                    filter_total += len(dim0)*len(dim1)
+                    total_sparsity += 1-(nz_count0*nz_count1)/(len(dim0)*len(dim1))
+                    total_layer += 1
+                
+        elt_sparsity = (total-nonzero)/total
+        input_sparsity = (filter_total-filter_count)/filter_total
+        output_sparsity = total_sparsity/total_layer
+
+        logger.info(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Sparsity elt: {elt_sparsity:.3f} str: {input_sparsity:.3f} str_avg: {output_sparsity:.3f}'
+              .format(top1=top1, top5=top5, elt_sparsity=elt_sparsity, input_sparsity=input_sparsity, output_sparsity=output_sparsity))
+
+    return top1.avg, top5.avg
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
@@ -349,7 +467,7 @@ class AverageMeter(object):
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
+    lr = args.lr * (args.lr_decay ** (epoch // args.lr_int))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
